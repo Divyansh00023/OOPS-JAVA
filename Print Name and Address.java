00509124b00abc74694852af1a647d5223925ba6
Divyansh00023 <divyansh.saxena.cs28@iilm.edu>
2025-08-23 15:21:07 +0530

    Create Print Name and Address.java

---
+++ Print Name and Address.java
@@ -0,0 +1,9 @@
+// To print Name and Address
+
+class main{
+    public static void main(String[] args){
+        System.out.println("My name is Divyansh Saxena.");
+        System.out.println("Address:- Greater Noida");
+        System.out.println("Pin:- 201310");
+    }
+}
